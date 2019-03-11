92f7d356da99b186ad15bacb003a89baa6857382
Eric Gjertsen <ericgj72@gmail.com>
2019-03-10 23:36:11 -0400

    deploy: do not pre-build and test development
    Note: assumes `gaetool build` is run prior to deploy, managed via make
    dependencies or similar.

--- gaetool/cmd/deploy.py
+++ gaetool/cmd/deploy.py
@@ -2,27 +2,24 @@ import os
 import subprocess
 
 from ._config import read_project_id
-from .build import build, build_lint, build_test
+from .build import build, build_lint
 
 def run(log, args):
     with log('deploy', env=args.env, service=args.service):
-        build(env='development', service=args.service, log=log)
-        build_lint(log=log)
-        build_test('development', args.service, log=log)
-
         build(env=args.env, service=args.service, log=log)
-        deploy(args.env, args.service, config=args.config, log=log)
+        build_lint(log=log, build_dir=args.build_dir)
+        deploy(args.env, args.service, config=args.config, build_dir=args.build_dir, log=log)
 
-def deploy(env, service, *, log, config="*.yaml"):
+def deploy(env, service, *, log, config="*.yaml", build_dir="build"):
     with log('app deploy: %s %s' % (env,service), env=env, service=service):
-        gcloud_app_deploy( read_project_id(env), env, config )
+        gcloud_app_deploy( read_project_id(env), env, config, build_dir )
 
 
-def gcloud_app_deploy(project, version, config):
+def gcloud_app_deploy(project, version, config, build_dir):
     if os.name == 'nt':
-        configfile = "build\\%s" % (config,)
+        configfile = "%s\\%s" % (build_dir, config)
     else:
-        configfile = "build/%s" % (config,)
+        configfile = "%s/%s" % (build_dir, config)
 
     cmd = """
         gcloud app deploy --no-promote --project="{project}" --version="{version}" {configfile}
